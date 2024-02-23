440aa01b1126d3d00a8194e6e94d2623e34d5f5d
acjunior007 <junior.leticia@gmail.com>
2024-02-23 17:59:26 -0300

    Configuration HttpClient.

--- GeekShopping/GeekShoppingProductAPI/obj/Debug/net6.0/GeekShoppingProductAPI.AssemblyInfo.cs
+++ GeekShopping/GeekShoppingProductAPI/obj/Debug/net6.0/GeekShoppingProductAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeekShoppingProductAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fe926b551ffdc2b7e74ab19df16c36ee43d72b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55171603ea8c013539530277a3accd928721b279")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeekShoppingProductAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeekShoppingProductAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
