8ebf9a6a4e29d97cf9e08832b084154653689d99
acjunior007 <junior.leticia@gmail.com>
2024-02-23 20:21:07 -0300

    Create List and create Product Views.

---
+++ GeekShopping/GeekShopping.Web/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using GeekShopping.Web.Models;
+using GeekShopping.Web.Services;
+using GeekShopping.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet(Name = "ListProduct")]
+        public async Task<IActionResult> ProductIndex()
+        {
+            var products = await _productService.FindAllProducts();
+            return View(products);
+        }
+
+        public async Task<IActionResult> ProductCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductCreate(ProductModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productService.CreateProduct(model);
+                if (response != null) return RedirectToAction("ProductIndex");
+            }
+
+            return View(model);
+        }
+    }
+
+}
--- GeekShopping/GeekShopping.Web/obj/Debug/net6.0/GeekShopping.Web.AssemblyInfo.cs
+++ GeekShopping/GeekShopping.Web/obj/Debug/net6.0/GeekShopping.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeekShopping.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55171603ea8c013539530277a3accd928721b279")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+440aa01b1126d3d00a8194e6e94d2623e34d5f5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeekShopping.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeekShopping.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
