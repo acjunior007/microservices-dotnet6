666e468452564a177ea98c8cfdeddac2497aee23
acjunior007 <junior.leticia@gmail.com>
2024-02-23 17:11:52 -0300

    Create Product Service.

---
+++ GeekShopping/GeekShopping.Web/Services/Interfaces/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using GeekShopping.Web.Models;
+
+namespace GeekShopping.Web.Services.Interfaces
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductModel>> FindAllProducts();
+
+        Task<ProductModel> FindProductById(long id);
+
+        Task<ProductModel> CreateProduct(ProductModel model);
+
+        Task<ProductModel> UpdateProduct(ProductModel model);
+
+        Task<bool> DeleteProduct(long id);
+    }
+}
--- GeekShopping/GeekShopping.Web/obj/Debug/net6.0/GeekShopping.Web.AssemblyInfo.cs
+++ GeekShopping/GeekShopping.Web/obj/Debug/net6.0/GeekShopping.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeekShopping.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9bb8aad156a4914f73ed56bf15770befc00cee9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+229ed04448b7d3ca76fdf1c9235989c390ca6794")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeekShopping.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeekShopping.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
