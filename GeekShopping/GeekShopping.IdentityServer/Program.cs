ea975278824920af752f30959c1086104bece6e1
acjunior007 <junior.leticia@gmail.com>
2024-02-27 20:03:33 -0300

    Identity services

---
+++ GeekShopping/GeekShopping.IdentityServer/Program.cs
@@ -0,0 +1,61 @@
+using GeekShopping.IdentityServer.Configuration;
+using GeekShopping.IdentityServer.Model.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddDbContext<MySQLContext>(options =>
+	options.UseMySql(
+		builder.Configuration.GetConnectionString("mySqlConn"),
+		new MySqlServerVersion(new Version(8, 0, 5)))
+);
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+	.AddEntityFrameworkStores<MySQLContext>()
+	.AddDefaultTokenProviders()
+	;
+
+builder.Services.AddIdentityServer(options =>
+{
+	options.Events.RaiseErrorEvents = true;
+	options.Events.RaiseInformationEvents= true;
+	options.Events.RaiseFailureEvents= true;
+	options.Events.RaiseSuccessEvents= true;
+	options.EmitStaticAudienceClaim = true;
+})
+	.AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+	.AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
+	.AddInMemoryClients(IdentityConfiguration.Clients)
+	.AddAspNetIdentity<ApplicationUser>()
+	;
+
+//ilder.Services.AddDefaultIdentity
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Home/Error");
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseIdentityServer();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
--- GeekShopping/GeekShopping.Web/obj/Debug/net6.0/GeekShopping.Web.AssemblyInfo.cs
+++ GeekShopping/GeekShopping.Web/obj/Debug/net6.0/GeekShopping.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GeekShopping.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c8b950aec68c27da2a250db2c60319d096b27bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fba46589a5186b5bb021a79a72dd132756cc70c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("GeekShopping.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GeekShopping.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
